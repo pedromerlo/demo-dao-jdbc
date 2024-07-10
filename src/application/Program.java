0572100f4bce52ecbb4b92634f188dcc026e67c8
pedromerlo <pedro.merlo.junior@gmail.com>
2024-07-10 12:56:15 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,5 +30,12 @@ public class Program {
         Seller newSeller = new Seller(null, "Greg", "gerg@gmail.com", new Date(), 4000.0, d);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId() );
+        
+        System.out.println("\n=== TEST 5: seller insert ===");
+        seller  = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        seller.setEmail("martha@gmail.com");
+        sellerDao.update(seller);
+        System.out.println("Uptade completed!");
     }
 }
