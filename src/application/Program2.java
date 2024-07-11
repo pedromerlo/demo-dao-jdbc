fc8da2ceade414d72b34ea8263b3a3e24d8c7d59
pedromerlo <pedro.merlo.junior@gmail.com>
2024-07-11 11:10:23 -0300

    Department findAll implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.DepartamentDao;
 import model.entities.Department;
@@ -9,7 +11,12 @@ public class Program2 {
         DepartamentDao departamentDao = DaoFactory.createDepartmentDao();
         Department department  = departamentDao.findById(2);
         System.out.println(department);
-
         
+        System.out.println("\n=== TEST 2: department findAll ===");
+        List<Department> list = departamentDao.findAll();
+        for (Department obj : list) {
+            System.out.println(obj);
+        }
+
     }
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -4,18 +4,22 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
 import model.dao.DepartamentDao;
 import model.entities.Department;
+import model.entities.Seller;
 
-public class DepartmentDaoJDBC implements DepartamentDao{
+public class DepartmentDaoJDBC implements DepartamentDao {
     private Connection conn;
-    
-    public DepartmentDaoJDBC(Connection conn){
-        this.conn =  conn;
+
+    public DepartmentDaoJDBC(Connection conn) {
+        this.conn = conn;
     }
 
     @Override
@@ -48,7 +52,7 @@ public class DepartmentDaoJDBC implements DepartamentDao{
             st.setInt(1, id);
             rs = st.executeQuery();
             if (rs.next()) {
-                Department dep = new Department();
+                Department dep = instatiateDepartment(rs);
                 dep.setId(rs.getInt("id"));
                 dep.setName(rs.getString("name"));
                 return dep;
@@ -65,8 +69,33 @@ public class DepartmentDaoJDBC implements DepartamentDao{
 
     @Override
     public List<Department> findAll() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try {
+            st = conn.prepareStatement(
+                    "SELECT * "
+                            + "FROM department "
+                            + "ORDER BY Name");
+            rs = st.executeQuery();
+            List<Department> list = new ArrayList<>();
+            while (rs.next()) {
+                Department obj = instatiateDepartment(rs);
+                list.add(obj);
+            }
+            return list;
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
 
+    private Department instatiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("id"));
+        dep.setName(rs.getString("name"));
+        return dep;
+    }
 }
